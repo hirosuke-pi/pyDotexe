7090c4e97e46aae805ad6900b41c1c63113c6feb
betacode_ <betacode.net@gmail.com>
2017-10-14 11:49:12 +0900

    Public_

---
+++ Sources/pyDotexe/MergeSet.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pyDotexe
+{
+    public class MergeSet
+    {
+        public string src_zip = "";
+        public string start_bin = "";
+        public string start_src = "";
+        public string start_argv = "";
+        public string version = "00";
+        public string icon_path = "";
+        public string output_path = "";
+        public bool use_cache = true;
+        public bool onefile = true;
+
+        public bool Decision()
+        {
+            // Found source file.
+            if (src_zip == Path.GetFileName(src_zip))
+                src_zip = Environment.CurrentDirectory + @"\" + src_zip;
+            if (start_src == "") start_src = start_bin;
+            if (!onefile) use_cache = false;
+
+            // Is Zip file?
+            if (!File.Exists(src_zip) & (Path.GetFileNameWithoutExtension(src_zip).ToLower() != ".zip") & onefile)
+            {
+                Console.WriteLine("[-] 選択されたファイルはZIPファイルでないか、ファイルが見つかりません。");
+                return false;
+            }
+            // Set icon.
+            if (icon_path == Path.GetFileName(icon_path))
+            {
+                if (File.Exists(Path.GetDirectoryName(src_zip) + @"\" + icon_path))
+                    icon_path = Path.GetDirectoryName(src_zip) + @"\" + icon_path;
+                else
+                    icon_path = "";
+            }
+            if (output_path == "")
+                if (onefile)
+                    output_path = Path.GetDirectoryName(src_zip) + @"\" + Path.GetFileNameWithoutExtension(start_src) + ".exe";
+                else
+                {
+                    output_path = src_zip + @"\" + Path.GetFileNameWithoutExtension(start_src) + ".exe";
+                    if (!Directory.Exists(Path.GetDirectoryName(output_path))) new DirectoryInfo(output_path).Create();
+                }
+            else
+                if (output_path == Path.GetFileName(output_path))
+                output_path = Path.GetDirectoryName(src_zip) + @"\" + output_path;
+            return true;
+        }
+
+        public MergeSet ConvertMergeSet(BuildSet bset)
+        {
+            MergeSet mset = new MergeSet();
+            mset.src_zip = bset.zip_path;
+            mset.start_bin = Path.GetFileName(bset.default_python_bin);
+            mset.start_src = Path.GetFileNameWithoutExtension(bset.source_path) + bset.default_src_ex;
+            mset.start_argv = bset.default_argv;
+            mset.version = bset.py_version;
+            mset.icon_path = bset.icon_path;
+            mset.output_path = bset.output_path;
+            mset.use_cache = bset.cache;
+            mset.onefile = bset.one_file;
+            mset.Decision();
+
+            return mset;
+        }
+    }
+}
---
+++ Sources/pyDotexe/Merger/Merge.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pyDotexe.Merger
+{
+    class Merge
+    {
+        private static MergeSet mset;
+        private class StartMergeError : Exception
+        {
+            public StartMergeError(string Message) { }
+        }
+
+        /// <summary>
+        /// Start merge Application
+        /// </summary>
+        /// <param name="MergeSettings">MergeSet Object</param>
+        /// <returns></returns>
+        public static bool Start(ref MergeSet MergeSettings)
+        {
+            try
+            {
+                mset = MergeSettings;
+                Console.WriteLine("[+] アプリケーションデータと結合しています...");
+                start_merge(); // Start merge
+                return true;
+            }
+            catch (StartMergeError)
+            {
+                return false;
+            }
+        }
+
+        public static bool Start(BuildSet BuildSettings)
+        {
+            try
+            {
+                mset = new MergeSet().ConvertMergeSet(BuildSettings);
+                Console.WriteLine("[+] アプリケーションデータと結合しています...");
+                start_merge(); // Start merge
+                return true;
+            }
+            catch (StartMergeError)
+            {
+                return false;
+            }
+        }
+
+        private static byte[] build_header()
+        {
+            // Build header
+            byte[] header = Encoding.UTF8.GetBytes(mset.start_src + "|" + mset.start_bin + "|" + mset.version + "|" + mset.start_argv);
+            return Enumerable.Concat(new byte[] { 0xff }, Encoding.UTF8.GetBytes(Convert.ToBase64String(header))).ToArray();
+            
+        }
+
+        private static void start_merge()
+        {
+            try
+            {
+
+                using (FileStream fs = new FileStream(mset.output_path, FileMode.Create, FileAccess.Write))
+                {
+                    if (!mset.onefile)
+                        fs.Write(Properties.Resource.Application_f, 0, Properties.Resource.Application_f.Length);
+                    else if (mset.use_cache)
+                        fs.Write(Properties.Resource.Application, 0, Properties.Resource.Application.Length);
+                    else
+                        fs.Write(Properties.Resource.Application_, 0, Properties.Resource.Application_.Length);
+                }
+
+                try
+                {
+                    if (mset.icon_path != "")
+                    {
+                        IconChanger icon_change = new IconChanger();
+                        icon_change.ChangeIcon(mset.output_path, mset.icon_path);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("[!] アイコンを変更で決ませんでした: " + ex.Message);
+                }
+
+                // Start merging
+                if (mset.onefile)
+                {
+                    byte[] before_data = null;
+                    using (FileStream fs = new FileStream(mset.output_path, FileMode.Open, FileAccess.Read))
+                    {
+                        before_data = new byte[fs.Length];
+                        fs.Read(before_data, 0, (int)fs.Length);
+                    }
+                    using (FileStream fs = new FileStream(mset.output_path, FileMode.Create, FileAccess.Write))
+                    {
+                        // Write all bytes
+                        fs.Write(before_data, 0, before_data.Length);
+                        FileStream fsm = new FileStream(mset.src_zip, FileMode.Open, FileAccess.Read);
+                        byte[] zip_bytes = new byte[fsm.Length];
+
+                        // Build header
+                        byte[] start_path = build_header();
+
+                        fsm.Read(zip_bytes, 0, (int)fsm.Length);
+                        fs.Write(zip_bytes, 0, zip_bytes.Length);
+                        fs.Write(start_path, 0, start_path.Length);
+                        fsm.Close();
+                    }
+                }
+                else
+                {
+                    byte[] before_data = null;
+                    using (FileStream fs = new FileStream(mset.output_path, FileMode.Open, FileAccess.Read))
+                    {
+                        before_data = new byte[fs.Length];
+                        fs.Read(before_data, 0, (int)fs.Length);
+                    }
+                    using (FileStream fs = new FileStream(mset.output_path, FileMode.Create, FileAccess.Write))
+                    {
+                        // Write all bytes
+                        fs.Write(before_data, 0, before_data.Length);
+                        // Build header
+                        byte[] header = build_header();
+                        fs.Write(header, 0, header.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\r\n[-] アプリケーションデータとの結合に失敗しました。");
+                Console.WriteLine("[!] " + ex.Message);
+                throw new StartMergeError("");
+            }
+            // Set time
+            try
+            {
+                File.SetCreationTime(mset.output_path, DateTime.Now);
+                File.SetLastAccessTime(mset.output_path, DateTime.Now);
+            }
+            catch { }
+        }
+    }
+}
